b64272dea9415ab32d2efe8ec8759a64f0691eb5
farrahpepino <idcfarrah@gmail.com>
2025-12-18 12:29:50 -0500

    cc

--- Employees2.py
+++ Employees2.py
@@ -1,3 +1,16 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.ml.feature import Imputer
+
+spark = SparkSession.builder.appName('Employees 2').getOrCreate()
+
+df = spark.read.csv('data/Employees2.csv', inferSchema=True, header=True)
+
+df.show()
+
+df.groupBy('Name').sum().show()
+df.groupBy('Departments').sum().show()
+df.agg({'Salary': 'sum'})
+df.groupBy('Departments').mean().show()
+df.groupBy('Departments').avg().show()
+df.groupBy('Name').min().show()
+df.groupBy('Name').max().show()
